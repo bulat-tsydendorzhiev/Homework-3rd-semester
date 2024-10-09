b055b6458b269a40d1ff1e201797bbd6edda4cdb
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2024-10-09 23:46:11 +0300

    feat: add interface with docs

---
+++ MyThreadPool/MyThreadPool/IMyTask.cs
@@ -0,0 +1,32 @@
+// <copyright file="IMyTask.cs" company="Bulat Tsydendorzhiev">
+// Copyright (c) Bulat Tsydendorzhiev. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// </copyright>
+
+namespace MyThreadPool;
+
+/// <summary>
+/// Task that will be executed by thread pool.
+/// </summary>
+/// <typeparam name="TResult">The type of task result.</typeparam>
+public interface IMyTask<TResult>
+{
+    /// <summary>
+    /// Gets a value indicating whether the task has completed.
+    /// </summary>
+    public bool IsCompleted { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the result of the task.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">Throws when the function corresponding to the task has terminated with an exception.</exception>
+    public TResult Result { get; }
+
+    /// <summary>
+    /// Applies to the result of a given task and returns a new task accepted for execution.
+    /// </summary>
+    /// <typeparam name="TNewResult">The type of new result.</typeparam>
+    /// <param name="func">A function used to continue work with result.</param>
+    /// <returns>New task.</returns>
+    public TNewResult ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+}
---
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="MyThreadPool.cs" company="Bulat Tsydendorzhiev">
+// Copyright (c) Bulat Tsydendorzhiev. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// </copyright>
+
+namespace MyThreadPool;
+
+public class MyThreadPool
+{
+}
